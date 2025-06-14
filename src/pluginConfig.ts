5c6a769bd580939269b2598dd69071c82e1e3a9e
chris <root@ChrisDell.localdomain>
2025-06-14 08:59:18 +0200

    version incr

--- src/pluginConfig.ts
+++ src/pluginConfig.ts
@@ -2,7 +2,7 @@ import type { ExternalPluginConfig } from '@windy/interfaces';
 
 const config: ExternalPluginConfig = {
     name: 'windy-plugin-airspaces',
-    version: '0.2.10',
+    version: '0.2.11',
     icon: '✈',
     title: 'Airspaces',
     description: 'Shows airspaces from openaip.net',
